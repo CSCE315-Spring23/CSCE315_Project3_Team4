c97ee0f67e2203bc6fafe0cb2c0c926009ecdae7
Lucas Wilber <s109099@tamu.edu>
2023-04-04 14:20:57 -0500

    add changes to src/backend/app.js

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,13 +1,17 @@
 var createError = require('http-errors');
 var express = require('express');
+var { Pool } = require('pg');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var menuItemsRouter = require('./routes/menuItemsRouter')
 
 var app = express();
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,14 +25,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/menuitems', menuItemsRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
