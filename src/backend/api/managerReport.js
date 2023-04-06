341b7e332d494b0674ea5bdf4eff789004b462d1
Anh Nguyen <ng.huyanh.gd@gmail.com>
2023-04-06 14:39:33 -0500

    add sales report

--- src/backend/api/managerReport.js
+++ src/backend/api/managerReport.js
@@ -7,16 +7,43 @@ var pool = db.getPool();
 - X Report
 - Restock Report
 - Excess Report
+- Sales Report
 */
+
+/* Given a time window, display the sales by item from the order history */
+const getSalesReport = (request, response) => {
+    if (request.query["start"] == null) {
+        response.status(400).json("Missing start date");
+    } else if (request.query["end"] == null) {
+        response.status(400).json("Missing end date");
+    } else {
+        let startTime = request.query["start"] + " 00:00:00";
+        let endTime = request.query["end"] + " 23:59:59";
+        pool.query(
+            'SELECT menuitems.name as "Menu Item", COUNT(1) as "Quantity Sold" , ROUND(CAST(SUM(orderlineitems.menuprice) AS numeric), 2) as "Sales"\n' +
+                "FROM orderlineitems \n" +
+                "JOIN menuitems ON orderlineitems.menuitemID = menuitems.menuItemID JOIN orders ON orderlineitems.orderID = orders.orderID \n" +
+                "WHERE orders.ordertime BETWEEN '" +
+                startTime +
+                "' AND '" +
+                endTime +
+                "' GROUP BY menuitems.name",
+            (error, results) => {
+                if (error) {
+                    response.status(404).json("Error getting response");
+                } else response.status(200).json(results.rows);
+            }
+        );
+    }
+};
 
 const getXReport = (request, response) => {
     pool.query(
         "SELECT * FROM orders WHERE date_trunc('day', ordertime) = (SELECT max(date_trunc('day', ordertime)) FROM orders)",
         (error, results) => {
             if (error) {
-                throw error;
-            }
-            response.status(200).json(results.rows);
+                response.status(404).json("Error getting response");
+            } else response.status(200).json(results.rows);
         }
     );
 };
@@ -32,7 +59,6 @@ const getExcessReport = (request, response) => {
         let year = date_ob.getFullYear();
         startDate = request.query["start"] + " 00:00:00";
         let formattedDateTime = year + "-" + month + "-" + date + " 23:59:59";
-        console.log(formattedDateTime);
         pool.query(
             "select t2.ingredientid, t1.soldqty, t2.name, t2.currAmount, t2.unit, t2.minAmount, t2.cost from (select it.ingredientid, sum(it.qty) as soldqty from inventorytransactions as it where it.ordertime between '" +
                 startDate +
@@ -41,9 +67,8 @@ const getExcessReport = (request, response) => {
                 "' group by it.ingredientid) t1 RIGHT JOIN (select i.ingredientID, i.name, i.currAmount, i.unit, i.minAmount, i.cost from inventory as i) t2 ON t1.ingredientID = t2.ingredientID WHERE (t1.soldqty < (t2.currAmount + t1.soldqty) * 0.1) or (soldqty is NULL);",
             (error, results) => {
                 if (error) {
-                    throw error;
-                }
-                response.status(200).json(results.rows);
+                    response.status(404).json("Error getting response");
+                } else response.status(200).json(results.rows);
             }
         );
     }
@@ -55,9 +80,8 @@ const getRestockReport = (request, response) => {
         "SELECT * from inventory WHERE CURRAMOUNT < MINAMOUNT",
         (error, results) => {
             if (error) {
-                throw error;
-            }
-            response.status(200).json(results.rows);
+                response.status(404).json("Error getting response");
+            } else response.status(200).json(results.rows);
         }
     );
 };
@@ -65,4 +89,5 @@ const getRestockReport = (request, response) => {
 router.get("/x-report", getXReport);
 router.get("/restock-report", getRestockReport);
 router.get("/excess-report", getExcessReport);
+router.get("/sales-report", getSalesReport);
 module.exports = router;
