61114ef2a9bcba57a972ede2cc4fb13cd5d62124
Paul Shin <paulgers99@gmail.com>
2023-04-13 11:19:25 -0500

    navbar comp & multi pages

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,30 +1,36 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Combos from "./server-pages/Combos";
+import Entrees from "./server-pages/Entrees";
+import Drinks from "./server-pages/Drinks";
+import Sweets from "./server-pages/Sweets";
+import Sides from "./server-pages/Sides";
+import CurrentOrder from "./server-pages/CurrentOrder";
 import LoginButton from "./loginButton.js";
 import LogoutButton from "./logoutButton.js";
 
+import {
+    BrowserRouter as
+    Router,
+    Routes,
+    Route,
+
+} from "react-router-dom";
+
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-                <LoginButton />
-                <LogoutButton />
-            </header>
-
-        </div>
+        <Router>
+            <Routes>
+                <Route path = "/Combos" element = { <Combos/> } />
+                <Route path = "/Entrees" element = { <Entrees/> } />
+                <Route path = "/Drinks" element = { <Drinks/> } />
+                <Route path = "/Sweets" element = { <Sweets/> } />
+                <Route path = "/Sides" element = { <Sides/> } />
+                <Route path = "/CurrentOrder" element = { <CurrentOrder/> } />
+            </Routes>
+        </Router>
+            
     );
 }
 
