6e277473755fba7982b3a4d7b749aa168dc4284c
Paul Shin <paulgers99@gmail.com>
2023-04-13 12:53:55 -0500

    additional frontend functionality

---
+++ src/frontend/src/server-pages/Logout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import LogoutButton from "../components/logoutButton"
+import LogIn from "./Login"
+
+
+function LogOut() {
+  return (
+    <div>
+        <LogoutButton />
+    </div>
+  )
+}
+
+export default LogOut
