69ddda6ca5ad67ac11c2a2a02db6f7e7a246ed52
Caleb Villarreal <cvill100@tamu.edu>
2023-04-16 00:48:35 -0500

    rename

---
+++ src/frontend/src/server-pages/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Link} from "react-router-dom"
+import LoginButton from "../components/loginButton.js";
+
+function LogIn() {
+  return (
+    <div>
+        Log In!!!
+        <LoginButton />
+    </div>
+  )
+}
+
+export default LogIn
