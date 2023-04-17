e57d14d0e2ff664df04b195652807fe7efd88870
Caleb Villarreal <cvill100@tamu.edu>
2023-04-17 10:19:43 -0500

    changed index temp

--- src/frontend/src/index.js
+++ src/frontend/src/index.js
@@ -4,9 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Auth0Provider } from "@auth0/auth0-react";
+import Sales from './manager-pages/Sales';
 
 const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, '../../.env') });
+
 
 ReactDOM.render(
   <Auth0Provider
@@ -21,6 +23,12 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+
+/*
+ReactDOM.render(<Sales />,
+  document.getElementById('root')
+);
+*/
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
