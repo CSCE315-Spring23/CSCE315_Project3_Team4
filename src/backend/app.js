f8c8ac7bce6c2f50bed6de48cc20af79d88ab49e
Anh Nguyen <ng.huyanh.gd@gmail.com>
2023-04-19 08:58:24 -0500

    add cors

--- src/backend/app.js
+++ src/backend/app.js
@@ -25,6 +25,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
+
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/menuitems", menuItemsRouter);
