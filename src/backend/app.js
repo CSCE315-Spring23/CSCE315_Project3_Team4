add3058191bd8cb6d40ea0d66d7732b210db16cd
Anh Nguyen <ng.huyanh.gd@gmail.com>
2023-04-18 22:44:11 -0500

    add cors allow all origins

--- src/backend/app.js
+++ src/backend/app.js
@@ -4,6 +4,7 @@ var { Pool } = require("pg");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var cors = require("cors");
 
 var indexRouter = require("./api/index");
 var usersRouter = require("./api/users");
@@ -12,6 +13,7 @@ var ordersRouter = require("./api/ordersRouter");
 var managerReportRouter = require("./api/managerReport");
 
 var app = express();
+app.use(cors());
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -26,23 +28,23 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/menuitems", menuItemsRouter);
-app.use("/orders", ordersRouter)
+app.use("/orders", ordersRouter);
 app.use("/manager/report/", managerReportRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
 });
 
 module.exports = app;
