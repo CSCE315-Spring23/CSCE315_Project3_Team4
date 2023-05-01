fff97de0ba6a9ef1504e867e55843b76d9ad6a9d
Anh Nguyen <ng.huyanh.gd@gmail.com>
2023-05-01 03:44:51 -0500

    add menu item

--- src/backend/api/managerAction.js
+++ src/backend/api/managerAction.js
@@ -35,6 +35,47 @@ const updateInventory = (request, response) => {
             .json("Minimum amount and current amount is less than 0");
 };
 
+/* Add menu item */
+const addMenuItem = (request, response) => {
+    data = request.body;
+    menuname = data["name"]; // string
+    menuprice = data["menuprice"]; // float
+    menuclassid = data["classid"]; // int
+    if (
+        menuname.length > 0 &&
+        menuprice > 0 &&
+        menuclassid >= 1 &&
+        menuclassid <= 5
+    ) {
+        pool.query(
+            "select max(menuitemid) from menuitems",
+            (error, results) => {
+                if (error) {
+                    response.status(404).json("Error get a new menuitem id");
+                } else {
+                    menuitemid = results.rows[0]["max"] + 1;
+                    pool.query(
+                        `insert into menuitems values (${menuitemid}, '${menuname}', ${menuprice}, ${menuclassid})`,
+                        (error, results) => {
+                            if (error) {
+                                response
+                                    .status(400)
+                                    .json("Error create a new menu item");
+                            } else
+                                response
+                                    .status(200)
+                                    .json(
+                                        `Add menu item successfully: id=${menuitemid}, name=${menuname}, price=${menuprice}, classId=${menuclassid}`
+                                    );
+                        }
+                    );
+                }
+            }
+        );
+    } else response.status(400).json("Bad request");
+};
+
 router.post("/update-inventory", updateInventory);
+router.post("/add-menu-item", addMenuItem);
 
 module.exports = router;
