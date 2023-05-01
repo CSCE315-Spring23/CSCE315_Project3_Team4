71d31f1202f8f6b21b65a79a5299f563e5cddf63
Anh Nguyen <ng.huyanh.gd@gmail.com>
2023-05-01 04:28:26 -0500

    rewrite dbconn

--- src/backend/api/dbConn.js
+++ src/backend/api/dbConn.js
@@ -1,8 +1,8 @@
 var pg = require("pg");
 const path = require("path");
 // require('dotenv').config({path: path.resolve(__dirname, '../../.env')});
-// require("dotenv").config({ path: "/etc/secrets/.env" });
-require("dotenv").config();
+require("dotenv").config({ path: "/etc/secrets/.env" });
+// require("dotenv").config();
 
 console.log(process.env.PSQL_DATABASE);
 
