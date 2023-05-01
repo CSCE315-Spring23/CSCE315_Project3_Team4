71ba88c2fefd26758a1ab8c623c54d8b6b8768fd
Sid Prabhakaran <s2siddhu@gmail.com>
2023-05-01 11:54:25 -0500

    actually added routing to hosted backend instead of local hosting

--- src/frontend/src/server-pages/Home.js
+++ src/frontend/src/server-pages/Home.js
@@ -1,75 +1,82 @@
-import React, { useEffect, useState } from "react";
-import "../components/server.css"
-import Employee from "../components/serverEmployee"
-import NavBar from "../components/serverNavBar"
-import ManagerView from "../manager-pages/Home"
-import axios from "axios"
+import '../components/server.css'
 
-function Home({ userClass }) {
+import axios from 'axios'
+import React, {useEffect, useState} from 'react';
 
-    const [items, setItems] = useState([]);
-    const [orderView, setOrderView] = useState(false);
-    const [managerView, setManagerView] = useState(false);
-    var [currItemClass, setCurrItemClass] = useState(1);
-    const [currentOrder, setCurrentOrder] = useState([]);
+import Employee from '../components/serverEmployee'
+import NavBar from '../components/serverNavBar'
+import ManagerView from '../manager-pages/Home'
 
-    useEffect(() => {
-        fetchItems();
-    }, [])
+function Home({userClass}) {
+  const [items, setItems] = useState([]);
+  const [orderView, setOrderView] = useState(false);
+  const [managerView, setManagerView] = useState(false);
+  var [currItemClass, setCurrItemClass] = useState(1);
+  const [currentOrder, setCurrentOrder] = useState([]);
 
-    const fetchItems = async () => {
-        const response = await axios.get("http://localhost:8000/menuItems/?class=" + currItemClass);
-        setItems(response.data);
-        setOrderView(false);
+  useEffect(() => {
+    fetchItems();
+  }, [])
+
+  const fetchItems =
+      async () => {
+    const response = await axios.get(
+        'https://revs-grill-backend.onrender.com/menuItems/?class=' +
+        currItemClass);
+    setItems(response.data);
+    setOrderView(false);
+  }
+
+  useEffect(() => {
+    console.log(items);
+  }, [items])
+
+  const handleItemClick = (item) => {
+    setCurrentOrder([...currentOrder, item]);
+  };
+
+  useEffect(() => {
+    console.log(currentOrder);
+  }, [currentOrder])
+
+  function handleCurrItemClass(newClass) {
+    setCurrItemClass(newClass);
+  }
+
+  useEffect(() => {
+    fetchItems();
+    console.log('New Item Class Selected: %s', currItemClass);
+  }, [currItemClass])
+
+  function handleOrderView() {
+    setOrderView(true);
+  };
+
+  function handleManagerView() {
+    if (userClass == 1) {
+      setManagerView(true);
+    } else {
+      console.log(
+          'Unauthorized attepmt to access manager view. UserClass = ',
+          userClass);
     }
-
-    useEffect(() => {
-        console.log(items);
-    }, [items])
-
-    const handleItemClick = (item) => {
-        setCurrentOrder([...currentOrder, item]);
-    };
-
-    useEffect(() => {
-        console.log(currentOrder);
-    }, [currentOrder])
-
-    function handleCurrItemClass(newClass) {
-        setCurrItemClass(newClass);
-    }
-
-    useEffect(() => {
-        fetchItems();
-        console.log("New Item Class Selected: %s", currItemClass);
-    }, [currItemClass])
-
-    function handleOrderView() {
-        setOrderView(true);
-    };
-
-    function handleManagerView() {
-        if (userClass == 1) {
-            setManagerView(true);
-        } else {
-            console.log("Unauthorized attepmt to access manager view. UserClass = ", userClass);
-        }
-    };
+  };
 
     return (
         <div>
-            {!managerView && <div className="backsplash">
+            {!managerView && <div className='backsplash'>
 
                 <header>
-                    <NavBar change={handleCurrItemClass} orderView={handleOrderView} isManager={userClass == 1} managerView={handleManagerView} />
+                    <NavBar change={handleCurrItemClass} orderView={handleOrderView} isManager={userClass == 1} managerView={
+    handleManagerView} />
                 </header>
                 <main>
                     <Employee />
-                    {!orderView && <div className="POS-container">
-                        <div className="Menu-grid">
+                    {!orderView && <div className='POS-container'>
+                        <div className='Menu-grid'>
                             {items.map(item => (
-                                <div key={item.menuitemid} className="MenuItem-block">
-                                    <button className="Item-Button"
+                                <div key={item.menuitemid} className='MenuItem-block'>
+                                    <button className='Item-Button'
                                         onClick={() => handleItemClick(item)}>{item.name}
                                     </button>
 
@@ -84,7 +91,7 @@ function Home({ userClass }) {
                             {currentOrder && currentOrder.map((item) => (
                                 <div key={item.menuitemid} className="MenuItem-block">
                                     <p className="Item-Button">{item.name}</p>
-                                    <p className="Item-Price">${item.menuprice.toFixed(2)}</p>
+                                    <p className='Item-Price'>${item.menuprice.toFixed(2)}</p>
                                 </div>
                             ))}
                         </div>
